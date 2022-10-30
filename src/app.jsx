cf24519defae06a7395f982a37f1e3ddbdffed1d
Eunju03 <105638371+Eunju03@users.noreply.github.com>
2022-10-30 17:20:28 +0900

    add login and logout functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 import styles from './app.module.css';
 import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
 function App({authService}) {
   return (
     <div class={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login authService={authService}/>}>
+            {/* <Login authService={authService} /> */}
+          </Route>
+          <Route path="/maker" element={<Maker authService={authService}/>}>
+            {/* <Maker /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+	const navigate = useNavigate();
+	const goToMaker = (userId) => {
+		navigate('/maker', {state: {id:userId}});
+	};
 	const onLogin = (event) => {
 		authService //
-		.login(event.currentTarget.textContent)
-		.then(console.log)
+			.login(event.currentTarget.textContent)
+			.then(data =>  goToMaker(data.user.uid));
 	};
+
+	// 이미 로그인되어있을 때 홈을 누르면 로그인페이지말고 maker로 감
+	useEffect(() => {
+		authService.onAuthChange(user => {
+			user && goToMaker(user.id);
+		})
+	});
 	return (
 		<section className={styles.login}>
 			<Header />
